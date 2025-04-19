6e36d7708ea2dabb033c4adcaaa932cdbb23074d
Sovual <linzhenlee2004@gmail.com>
2025-04-19 15:53:05 +0800

    new dir

---
+++ Materials/BOSS-0.125/OPT/command-opt.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#建立QHA计算文件夹
+for i in $(seq 1.04 0.005 1.04)
+#对i赋值0.95到1.05之间，每隔0.005取值。
+do
+mkdir $i
+cp KPOINTS INCAR POTCAR POSCAR-unitcell opt_loop.sh ./$i
+#cp INCAR-OPT ./$i/INCAR
+cd ./$i
+sed '2c '$i'' POSCAR-unitcell > temp
+mv temp POSCAR
+bash opt_loop.sh
+#mv CONTCAR POSCAR
+#rm POSCAR
+cd ..
+done
+
+
+#执行QHA计算
